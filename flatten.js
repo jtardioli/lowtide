59c6058ee26d863350c57ca6b0ed9df6ccaab716
jtardioli <joshua.tardioli@gmail.com>
2021-10-26 16:13:01 -0400

    Add flatten.js

--- flatten.js
+++ flatten.js
@@ -45,5 +45,10 @@ const flatten = (array) => {
   return flattenedArray;
 };
 
+// return within for each
+// proper way to escape a function
+// pure functions
+// closures
+
 //console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
 console.log(flatten([1, 2, [3, 4], 5, [6]]));

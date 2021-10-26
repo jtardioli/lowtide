a204c6adb6484a716fb4752f4e9be21acf415a66
jtardioli <joshua.tardioli@gmail.com>
2021-10-26 16:12:26 -0400

    Add flatten.js

---
+++ flatten.js
@@ -0,0 +1,49 @@
+const eqArrays = (array1, array2) => {
+  if (array1.length === array2.length) {
+    for (let i = 0; i <= array1.length; i++) {
+      if (!(array1[i] === array2[i])) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArrayEqual = (array1, array2, expected) => {
+  const actual = eqArrays(array1, array2);
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const flatten = (array) => {
+  let flattenedArray = [];
+  const flattenInner = (array) => {
+    // Loop through every item of the array
+    array.forEach((item, index) => {
+      // if the item is an array recurse with that array
+      if (Array.isArray(item)) {
+        return flattenInner(item);
+      } else {
+        // otherwise push the item into the new array
+        flattenedArray.push(item);
+        // console.log(index, array.length - 1, item);
+        // console.log(flattenedArray);
+        // once the end of the array is reached return the final array
+        if (index === array.length - 1) {
+          //console.log("right before", flattenedArray);
+          return flattenedArray;
+        }
+      }
+    });
+  };
+  flattenInner(array);
+  return flattenedArray;
+};
+
+//console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+console.log(flatten([1, 2, [3, 4], 5, [6]]));

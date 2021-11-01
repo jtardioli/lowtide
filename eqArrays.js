4f774ad68f37fb77bc756725a2afb72cea8f22d9
jtardioli <joshua.tardioli@gmail.com>
2021-10-31 21:32:55 -0400

    Final commit

--- eqArrays.js
+++ eqArrays.js
@@ -19,6 +19,35 @@ const assertEqual = function(actual, expected) {
 //   return true;
 // };
 
+// const eqArrays = (array1, array2) => {
+//   // check if arrays are the same length
+//   if (array1.length !== array2.length) {
+//     return false;
+//   }
+//   for (let i in array1) {
+//     // check if index i is an array
+//     if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+//       // if it is loop through it, if it returns false then return false for enrtire function
+//       if (!eqArrays(array1[i], array2[i])) {
+//         return false;
+//       }
+//     } else {
+//       // check if one is an array but not both
+//       if ((Array.isArray(array1[i]) === false || Array.isArray(array2[i]) === false) && !(Array.isArray(array1[i]) === false && Array.isArray(array2[i]) === false)) {
+//         return false;
+//       }
+
+//       console.log(array1[i], array2[i]);
+//       // check if index 1 = 2
+//       if (!array1[i] === array2[i]) {
+//         return false;
+//       }
+//     }
+    
+//   }
+//   return true;
+// };
+
 const eqArrays = (array1, array2) => {
   // check if arrays are the same length
   if (array1.length !== array2.length) {
@@ -33,15 +62,14 @@ const eqArrays = (array1, array2) => {
       }
     } else {
       // check if one is an array but not both
-      if ((Array.isArray(array1[i]) === false || Array.isArray(array2[i]) === false) && !(Array.isArray(array1[i]) === false && Array.isArray(array2[i]) === false)) {
+      const item1IsArray = Array.isArray(array1[i]);
+      const item2isArray = Array.isArray(array2[i]);
+      if (
+        (item1IsArray || item2isArray) &&
+        !(item1IsArray && item2isArray)) {
         return false;
       }
 
-      console.log(array1[i], array2[i]);
-      // check if index 1 = 2
-      if (!array1[i] === array2[i]) {
-        return false;
-      }
     }
     
   }
